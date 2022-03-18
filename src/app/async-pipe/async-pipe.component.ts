c73ea5d8ea210c29e94cacee5afc25750f11c9ff
janitha000 <janitha000@gmail.com>
2022-03-18 13:48:33 +0530

    add keyvalue pipe

--- src/app/async-pipe/async-pipe.component.ts
+++ src/app/async-pipe/async-pipe.component.ts
@@ -13,7 +13,9 @@ import { Observable, of, shareReplay } from 'rxjs';
     <h4>NgFor </h4>
     <ul>
   <li *ngFor="let breed of (dogsObs | async)?.message">{{breed}}</li>
-</ul>`
+</ul>
+ <p>Key Value Pipe </p>
+ <ul><li *ngFor="let item of user | keyvalue"> {{item.key}} --> {{item.value}}</li></ul>`
 })
 
 export class AsyncPipeComponent implements OnInit {
@@ -39,8 +41,21 @@ export class AsyncPipeComponent implements OnInit {
 
     dogs: dogs = { message: ["afghan", "basset", "blood"] }
     dogsObs = of(this.dogs);
+
+    //keyvalue pipe
+    //used when iterating through object properties (without having an array)
+    //default sorting is alphetic / or numerical if key is a number
+    //can give a custom sorting function
+    user: User = { name: 'Janitha', age: 31, city: 'Kandy' }
+
 }
 
 interface dogs {
     message: string[]
+}
+
+interface User {
+    name: string,
+    age: number,
+    city: string
 }
