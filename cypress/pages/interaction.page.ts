3c04ac1bef3f5dd2f6a9c5f95f528c8b98af0e26
janitha000 <janitha000@gmail.com>
2022-03-22 18:29:30 +0530

    Add cypress e2e tests

---
+++ cypress/pages/interaction.page.ts
@@ -0,0 +1,17 @@
+export class Interaction {
+    public visit(): void {
+        cy.visit('/component-interaction')
+    }
+
+    public clickChild(): void {
+        cy.contains('Change Child Var').click();
+    }
+
+    public sendMessage(): void {
+        cy.contains('Send Message Child').click();
+    }
+
+    public getByTestId(id: string): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.byTestId(id);
+    }
+}
