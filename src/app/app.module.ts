75f290e78deb4c56e029b1eac82ea62bb59a0e88
janitha000 <janitha000@gmail.com>
2022-03-29 10:59:44 +0530

    add more dynamic loading examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { StyleComponent, StyleParentComponent } from './component-styles/style.c
 import { CardComponent, ContentProjectionComponent, ContentProjectionParentComponent } from './content-projection/content-projection.component';
 import { CustomPipeComponent } from './custom-pipe/custom-pipe.component';
 import { ExponentialPipe } from './custom-pipe/exponential.pipe';
-import { DynamicComponent, DynamicOneComponent, DynamicTwoComponent } from './dynamic-loading/dynamic-loading.component';
+import { DynamicComponent, DynamicFactoryComponent, DynamicOneComponent, DynamicTwoComponent } from './dynamic-loading/dynamic-loading.component';
 import { DynamicDirective } from './dynamic-loading/dynamic.directive';
 import { HomeComponent } from './home/home.component';
 import { LifecycleComponent, LifecycleParentComponent } from './lifecycle/lifecycle.component';
@@ -73,6 +73,7 @@ import { ViewChidComponent, ViewParentComponent } from './view-child/view-child.
     ViewChidComponent,
     ViewParentComponent,
     CardComponent,
+    DynamicFactoryComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, CommonModule, FormsModule, RouterModule, ReactiveFormsModule],
   providers: [InteractionService],
--- src/app/dynamic-loading/dynamic-loading.component.ts
+++ src/app/dynamic-loading/dynamic-loading.component.ts
@@ -1,11 +1,13 @@
-import { Component, Input, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { Component, ComponentFactoryResolver, Input, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 import { DynamicDirective } from './dynamic.directive';
 
 @Component({
   selector: 'app-dynamic',
   template: `<h2>Dynamic Loading Components</h2>
     <button (click)="onClick()">Click to change Comp</button>
-    <ng-template dynamicHost></ng-template> `,
+    <ng-template dynamicHost></ng-template>
+    <p>-------------------------</p>
+    <app-dynamic-factory></app-dynamic-factory>`,
 })
 export class DynamicComponent implements OnInit {
   @ViewChild(DynamicDirective, { static: true }) dynamicHost!: DynamicDirective;
@@ -31,6 +33,29 @@ export class DynamicComponent implements OnInit {
 
   ngOnInit() {
     this.loadComponent(this.dynamicItems[0]);
+  }
+}
+
+//same dynamic loading using viewContainerRef using template variables
+@Component({
+  selector: 'app-dynamic-factory',
+  template: `<h2>Dynamic Loading Components</h2>
+    <button (click)="onClick()">Click to change Comp</button>
+    <ng-template #compHost></ng-template> `,
+})
+export class DynamicFactoryComponent implements OnInit {
+  @ViewChild('compHost', { static: true, read: ViewContainerRef }) container!: ViewContainerRef;
+  changeComponent: boolean = true;
+  constructor(private resolver: ComponentFactoryResolver) {}
+
+  ngOnInit() {}
+
+  onClick() {
+    let comp = this.changeComponent ? DynamicOneComponent : DynamicTwoComponent;
+
+    this.container.clear();
+    const componentRef = this.container.createComponent(comp);
+    componentRef.instance.data = 'Janitha';
   }
 }
 
